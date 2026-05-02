99bdd2e13667b5e4845ef2bea3b9f5c0fd2b14d6
Влад <vlados@example.com>
2026-05-02 23:51:19 +0300

    fix allowed hosts

--- settings.py
+++ settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-$^rx6_jml%-^optj+6n*h$kv58x27fhcr8=ixapmdc7ndb+6@k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['shop-n3qe.onrender.com']
 
 
 # Application definition
